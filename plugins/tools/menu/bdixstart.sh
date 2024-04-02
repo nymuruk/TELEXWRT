1ac3f454f34074b0dcdec73128a44b03a8d4ac3c
Nymur Rahman <nymurrahmanbd@gmail.com>
2024-04-02 22:58:16 +0600

    BDIX Added 😘

---
+++ plugins/tools/menu/bdixstart.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+service bdix start
+
+# READ AUTH
+if [ -f "/root/TELEXWRT/AUTH" ]; then
+    IFS=$'\n' read -d '' -r -a lines < "/root/TELEXWRT/AUTH"
+    if [ "${#lines[@]}" -ge 2 ]; then
+        BOT_TOKEN="${lines[0]}"
+        CHAT_ID="${lines[1]}"
+    else
+        echo "Auth file must have at least 2 lines (your token and chat ID)."
+        exit 1
+    fi
+else
+    echo "Auth file not found."
+    exit 1
+fi
+
+# Create notification message
+MSG="bdix success to start!"
+
+# Send notification message to Telegram bot
+curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID" -d "text=$MSG"
